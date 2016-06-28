0acdbe86e282b4fef13be75efdc376e8fe873377
Paul Savignano <Paul.Savignano@gmail.com>
2016-06-27 17:25:25 -0700

    Added components, collection, methods, and publications.

---
+++ client/components/bins/bins_list.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { createContainer } from 'meteor/react-meteor-data'
+import { Link } from 'react-router'
+
+import { Bins } from '../../../imports/collections/bins'
+
+class BinsList extends Component {
+  onBinRemove(bin) {
+    Meteor.call('bins.remove', bin._id)
+  }
+  renderList() {
+    return this.props.bins.map((bin) => {
+      const url = `bins/${bin._id}`
+      return (
+        <li key={bin._id} className="list-group-item">
+          <Link to={url}>Bin: {bin._id}</Link>
+          <span className="pull-right">
+            <button
+              onClick={() => this.onBinRemove(bin)}
+              className="btn btn-danger"
+            >
+              Remove
+            </button>
+          </span>
+        </li>
+      )
+    })
+  }
+  render() {
+    return (
+      <ul className="list-group">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}
+
+export default createContainer(() => {
+  Meteor.subscribe('bins')
+  Meteor.subscribe('sharedBins')
+  return { bins: Bins.find({}).fetch() }
+}, BinsList)
---
+++ client/components/header.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Link, browserHistory } from 'react-router'
+
+import Accounts from './accounts'
+
+class Header extends Component {
+  onBinClick(event) {
+    event.preventDefault()
+    Meteor.call('bins.insert', (error, result) => {
+      browserHistory.push(`bins/${result}`)
+    })
+  }
+  render() {
+    return (
+      <nav className="nav navbar-default">
+        <div className="navbar-header">
+          <Link to="/" className="navbar-brand">Markbin</Link>
+        </div>
+        <ul className="nav navbar-nav">
+          <li>
+            <Accounts />
+          </li>
+          <li>
+            <a
+              href="#"
+              onClick={this.onBinClick.bind(this)}
+            >
+              Create Bin
+            </a>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Header
